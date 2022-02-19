7f65e4a5f5354efe4dbe14fe40f441dd199124c9
Vitória Souza <vitoria.souza@acct.global>
2022-02-19 09:19:59 -0300

    added component Layout and Title

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
     <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 via-orange-400 to-blue-600 ">
-      <span className="text-4xl"> Texto </span>
+      <Layout title="Cadastro Simples">
+        <span> Texto </span>
+      </Layout>
     </div>
   );
 }
