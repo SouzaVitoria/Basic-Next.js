81859c735961a7f0266097fd0b97a0b6bebbc8c8
Vitória Souza <vitoria.souza@acct.global>
2022-02-19 09:45:18 -0300

    Table component rendering

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const clients = [
+    new Client("Vitória", 21, "1"),
+    new Client("Vitória Souza", 22, "2"),
+    new Client("Danilo", 29, "3"),
+    new Client("Danilo Caraça", 30, "4"),
+  ];
+
   return (
     <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 via-orange-400 to-blue-600 ">
       <Layout title="Cadastro Simples">
-        <span> Texto </span>
+        <Table clients={clients} />
       </Layout>
     </div>
   );
