3d37a1f143a076327f98856b8720ae9679c1a4c2
Vitória Souza <vitoria.souza@acct.global>
2022-02-19 10:01:26 -0300

    Table component styles

--- next-crud/src/components/Table.tsx
+++ next-crud/src/components/Table.tsx
@@ -8,9 +8,9 @@ export default function Table(props: TableProps) {
   function renderHeader() {
     return (
       <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
       </tr>
     );
   }
@@ -18,18 +18,23 @@ export default function Table(props: TableProps) {
   function renderData() {
     return props.clients?.map((client, i) => {
       return (
-        <tr key={client.id}>
-          <td>{client.id}</td>
-          <td>{client.name}</td>
-          <td>{client.age}</td>
+        <tr
+          key={client.id}
+          className={`${i % 2 === 0 ? "bg-purple-300" : "bg-purple-200"} `}
+        >
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
         </tr>
       );
     });
   }
 
   return (
-    <table>
-      <thead>{renderHeader()}</thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead className="text-gray-100 bg-gradient-to-r from-purple-600 to-purple-900">
+        {renderHeader()}
+      </thead>
       <tbody>{renderData()}</tbody>
     </table>
   );
